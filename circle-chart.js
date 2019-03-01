c27d70e0ea72eed78afc8296f313b5fe4ea7152b
S1mpler <stas.dior@gmail.com>
2019-03-01 17:34:48 +0100

    Add selecting behaivor

--- circle-chart.js
+++ circle-chart.js
@@ -31,6 +31,7 @@ for (let i = 0; i < 100; i++) {
   let cRadius =(Math.random() * 10) + 5;
 
   const circle = {
+    id: i,
     x: ((width) / 2) + r * Math.cos(a),
     y: ((height) / 2) + r * Math.sin(a),
     r: cRadius / 1.5,
@@ -39,16 +40,23 @@ for (let i = 0; i < 100; i++) {
     velocity: (Math.random() * 0.014) + 0.01
   }
 
-  if (i === 90) {
-    circle.x = width / 2;
-    circle.y = height / 2;
-    circle.distance = 4;
-    circle.velocity = 0.02;
-    circle.r = 20;
-  }
+  // if (i === 90) {
+  //   circle.x = width / 2;
+  //   circle.y = height / 2;
+  //   circle.distance = 4;
+  //   circle.velocity = 0.02;
+  //   circle.r = 20;
+  // }
 
   circlesData.push(circle);
 }
+
+
+let randCircle = circlesData[Math.floor(Math.random() * 99) + 0]
+randCircle.oldDist = randCircle.distance;
+
+
+// D3
 
 let svg = d3.select('.chart')
   .append('svg')
@@ -76,38 +84,53 @@ let selector = svg
     .style("stroke-width", 1)
     .style("stroke", 'white')
     .style("fill-opacity", 0);
-
-// const randCircle = circlesData[Math.floor(Math.random() * 99) + 0]
+    
+let selectorLine = svg
+  .append('line')
+    .attr("x1", width/2 + 30 * Math.cos(315))
+    .attr("y1", height/2 + 30 * Math.sin(315))
+    .attr("x2", width - width / 7)
+    .attr("y2", height - height/7)
+    .style("stroke-width", 1)
+    .style("stroke", 'white');
 
 d3.interval(update, 10);
 
 function update() {
   circleAttributes
-  .attr("cx", function (d) {
-    d.radians += d.velocity;
-    d.x = width / 2 + Math.cos(d.radians) * d.distance;
-    return d.x;
-  })
-  .attr("cy", function (d) {
-    d.radians += d.velocity;
-    d.y = height / 2 + Math.sin(d.radians) * d.distance;
-    return d.y;
-  })
-  .attr("r", function (d) { 
-    d.r = d.r;
-    return d.r; 
-  });
+    .attr("cx", function (d) {
 
-  if (randCircle) {
-    selector
+      if (d.id === randCircle.id && d.distance > 4) d.distance -= d.distance / 100;
+
+      d.radians += d.velocity;
+      d.x = width / 2 + Math.cos(d.radians) * d.distance;
+      return d.x;
+    })
+    .attr("cy", function (d) {
+
+      if (d.id === randCircle.id && d.distance > 4) d.distance -= d.distance / 100;
+
+      d.radians += d.velocity;
+      d.y = height / 2 + Math.sin(d.radians) * d.distance;
+      return d.y;
+    });
+
+  selectorLine
+    .attr("x1", randCircle.x + (randCircle.r + 10) * Math.cos(315))
+    .attr("y1", randCircle.y + (randCircle.r + 10) * Math.sin(315))
+
+  selector
     .attr("cx", randCircle.x)
     .attr("cy", randCircle.y)
     .attr("r", randCircle.r + 10);
-  }
 }
 
-function distance(params) {
-// var a = x1 - x2;
-// var b = y1 - y2;
-// var c = Math.sqrt( a*a + b*b );
+document.body.onkeyup = function(e){
+  if (e.keyCode == 32){
+    e.preventDefault();
+    console.log('old', randCircle);
+    randCircle = circlesData[Math.floor(Math.random() * 99) + 0];
+    randCircle.oldDist = randCircle.distance;
+    console.log('new', randCircle);
+  }
 }
