f986732d05e42f4ff0964def311229110f49d1ab
S1mpler <stas.dior@gmail.com>
2019-04-15 12:32:49 +0200

    Please, fix the loundess, bc you broke it

--- circle-chart.js
+++ circle-chart.js
@@ -17,8 +17,8 @@ const getSongData = (circle) => circle.data;
 
 const shuffle = (a) => {
   for (let i = a.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [a[i], a[j]] = [a[j], a[i]];
+    const j = Math.floor(Math.random() * (i + 1));
+    [a[i], a[j]] = [a[j], a[i]];
   }
   return a;
 }
@@ -39,7 +39,7 @@ const pulseIn = (circle, pulseSpeed) => {
 }
 
 //TODO: remove
-const generatePropertyElement = (prop, score) => 
+const generatePropertyElement = (prop, score) =>
   `<div class="song-prop">
     ${prop}
     <div class="progress" style="width:${score * 100}%"></div>
@@ -51,39 +51,31 @@ const generatePropertyElement = (prop, score) =>
 ///////////////////////////////////////////////////  
 
 const danceability = (circle) => {
-    if(circle.id === randCircle.id) return;
-    console.log(danceabilityScale(randCircle.data.danceability))
-    circle.distance += (50 * Math.cos(danceabilityScale(randCircle.data.danceability) * circle.radians)) / 10;
-    circle.velocity = 0.009;
-    // else
-      // circle.distance = minDistance * Math.sin(6 * circle.radians);
+  if (circle.id === randCircle.id) return;
+  console.log(danceabilityScale(randCircle.data.danceability))
+  circle.distance += (25 * Math.sin(danceabilityScale(randCircle.data.danceability) * circle.radians)) / 10;
 }
 
 const loudness = (circle) => {
-  // if(circle.id !== randCircle.id){
-  if(circle.loudnessCounter == 0){
-    circle.r = circle.r * loudnessScale(randCircle.data.loudness);
+  if (circle.loudnessCounter == 0) {
+    circle.r = circle.r * 10//loudnessScale(randCircle.data.loudness);
     circle.loudnessCounter = circle.loudnessCounter + 1;
-  } else if(circle.loudnessCounter == 45){
+  } else if (circle.loudnessCounter == 45) {
     circle.r = circle.oldRad;
     circle.loudnessCounter += 1;
-  }else if(circle.loudnessCounter == 90){
+  } else if (circle.loudnessCounter == 90) {
     circle.loudnessCounter = 0;
-  }else{
+  } else {
     circle.loudnessCounter += 1;
   }
-// }
 }
 
 const valence = (circle) => {
   circle.color = colorScale(randCircle.data.valence)
-
 }
 
 const tempo = (circle) => {
-  // if(circle.id !== randCircle.id){
-    circle.tempo = tempoScale(randCircle.data.tempo)
-  // }
+  circle.tempo = tempoScale(randCircle.data.tempo)
 }
 
 ///////////////////////////////////////////////////
@@ -108,38 +100,38 @@ let selectedCircleIndex = 0;
 let isSpreaded = false;
 
 const colorScale = d3.scaleLinear()
-  .domain([0,1])
+  .domain([0, 1])
   .range([d3.rgb("#116f32"), d3.rgb('#77d598')])
 const loudnessScale = d3.scaleLinear()
-  .domain([-11,0])
-  .range([1.1,1.5]);
+  .domain([-11, 0])
+  .range([1.1, 1.5]);
 const tempoScale = d3.scaleLinear()
   .domain([60, 200])
-  .range([2,1]);
+  .range([2, 1]);
 const danceabilityScale = d3.scaleQuantize()
-  .domain([0,1])
-  .range([1,2,3,4, 5, 6, 7]);
+  .domain([0, 1])
+  .range([1, 3, 4, 5, 6]);
 for (let i = 0; i < songs.length; i++) {
   let a = Math.random() * 2 * Math.PI;
-  let r = ((width-offset) / 2) * Math.sqrt(Math.random());
-  let cRadius =(Math.random() * 10) + 5;
+  let r = ((width - offset) / 2) * Math.sqrt(Math.random());
+  let cRadius = (Math.random() * 10) + 5;
   let staticVelocity = ((Math.random() * 0.014) + 0.016) * 1.5;
 
   const circle = {
     id: songs[i].id,
     x: ((width) / 2) + r * Math.cos(a),
     y: ((height) / 2) + r * Math.sin(a),
-    r: cRadius / 1.5,
+    r: 7,//cRadius / 1.5,
     color: '#1DB954',
     opacity: 0.0,
-    distance: (Math.floor(Math.random() * (i * 1.0)) + minDistance),  
-    oldDist: (Math.floor(Math.random() * (i * 1.0)) + minDistance),  
+    distance: (minDistance),  //(Math.floor(Math.random() * (i * 1.0)) + minDistance)
+    oldDist: (minDistance),
     radians: Math.random() * Math.PI * 2,
     oldRad: cRadius / 1.5,
     velocity: staticVelocity,
     staticVelocity,
     data: songs[i],
-    tempo:1,
+    tempo: 1,
     loudnessCounter: 0,
     danceabilityCounter: 0,
     danceabilityBool: false
@@ -156,10 +148,10 @@ pickRandomCircle(selectedCircleIndex);
 // D3
 let svg = d3.select('.chart')
   .append('svg')
-    .attr('width', width + margin.left + margin.right)
-    .attr('height', height + margin.top + margin.bottom)
-    .append('g')
-      // .attr('transform', `translate(${margin.left}, ${margin.top})`)
+  .attr('width', width + margin.left + margin.right)
+  .attr('height', height + margin.top + margin.bottom)
+  .append('g')
+// .attr('transform', `translate(${margin.left}, ${margin.top})`)
 
 let circles = svg.selectAll('circle')
   .data(circlesData)
@@ -170,26 +162,26 @@ let circleAttributes = circles
   .attr("cx", function (d) { return d.x; })
   .attr("cy", function (d) { return d.y; })
   .attr("r", function (d) { return d.r; })
-  .attr("distance", function(d) { return d.distance})
-  .style("fill", function(d){return d.color})
-  .style("transition", function(d){return d.tempo});
-    
+  .attr("distance", function (d) { return d.distance })
+  .style("fill", function (d) { return d.color })
+  .style("transition", function (d) { return d.tempo });
+
 
 let selector = svg
   .append('circle')
-    .attr("cx", width/2)
-    .attr("cy", height/2)
-    .attr("r", 30)
-    .style("stroke-width", 1)
-    .style("stroke", 'white')
-    .style("fill-opacity", 0);
-    
+  .attr("cx", width / 2)
+  .attr("cy", height / 2)
+  .attr("r", 30)
+  .style("stroke-width", 1)
+  .style("stroke", 'white')
+  .style("fill-opacity", 0);
+
 let selectorLine = svg
   .append('line')
-    .attr("x2", width)
-    .attr("y2", height / 2)
-    .style("stroke-width", 1)
-    .style("stroke", 'white');
+  .attr("x2", width)
+  .attr("y2", height / 2)
+  .style("stroke-width", 1)
+  .style("stroke", 'white');
 
 update();
 
@@ -199,7 +191,7 @@ d3.interval(update, 9);
 
 function update() {
   circleAttributes
-    .each(function(d) {
+    .each(function (d) {
       valence(d);
       loudness(d);
       tempo(d);
@@ -212,7 +204,7 @@ function update() {
         if (isSpreaded && !pulseDone) {
           d.velocity -= d.velocity / 90;
           // pulseOut(d, d.distance / pulseExpandFactor)
-        } 
+        }
         // if (pulseDone && d.distance !== d.oldDist) {
         //   // moving back to the position
         //   if ( !isSpreaded) pulseIn(d, (d.distance - d.oldDist) / pulseCollapsFactor);
@@ -230,12 +222,13 @@ function update() {
       return d.x;
     })
     .attr("cy", function (d) {
-        d.y = height / 2 + Math.sin(d.radians) * d.distance;
+      d.y = height / 2 + Math.sin(d.radians) * d.distance;
       return d.y;
     })
     .attr('distance', d => Math.cos(5 * d.radians))
     .style('fill', d => d.color)
-    .style('transition', d=> 'r '+ d.tempo + 's,fill 1s ease-out, distance 2s ease');
+    .style('transition', d => 'r ' + d.tempo + 's');
+  // .style('transition', d=> 'r '+ d.tempo + 's, fill 1s ease-out');
 
   selectorLine
     .attr("x1", randCircle.x + (randCircle.r + 10) * Math.cos(0))
@@ -268,8 +261,8 @@ tutorialBtn.addEventListener('click', (e) => {
   tutorialPanel.style.visibility = 'visible'
   tutorialPanel.style.animation = '1s ease-out 0s 1 slideInFromLeft';
 })
-document.body.onkeyup = function(e){
-  if (e.keyCode == 32){
+document.body.onkeyup = function (e) {
+  if (e.keyCode == 32) {
     e.preventDefault();
     pickRandomCircle();
   }
@@ -306,10 +299,10 @@ function pickRandomCircle() {
 function dispaySongData(data) {
   songNameDiv.innerText = `${data.artists} - ${data.name}`;
   songDataDiv.innerHTML = `
-    ${generatePropertyElement('danceability',data.danceability)}
-    ${generatePropertyElement('loudness',data.loudness)}
-    ${generatePropertyElement('valence',data.valence)}
-    ${generatePropertyElement('tempo',data.tempo)}
+    ${generatePropertyElement('danceability', data.danceability)}
+    ${generatePropertyElement('loudness', data.loudness)}
+    ${generatePropertyElement('valence', data.valence)}
+    ${generatePropertyElement('tempo', data.tempo)}
   `;
   // -\n
   // energy\t\t-\t${data.energy}\n
